8c7c4033abf718d387b687b1ba1554b79b9eea5b
sudo0272 <sudo180272@protonmail.ch>
2020-08-14 17:23:32 +0900

    Solve RemoveDuplicatesFromSortedArray of Leetcode/TopInterviewQuestions/Easy/Array

---
+++ LeetCode/TopInterviewQuestions/Easy/Array/RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    int removeDuplicates(std::vector<int>& nums) {
+      unsigned int targetIndex = 1; // I have to replace numbers from 1
+                                    // because the algorithm below uses nums[0]
+                                    // as the standard of comparison at first loop
+
+      for (unsigned int i = 1; i < nums.size(); i++) {
+        if (nums[i - 1] != nums[i]) {
+          nums[targetIndex] = nums[i];
+
+          targetIndex++;
+        }
+      }
+
+      return targetIndex; // targetIndex is the position of next element
+                          // so targetIndex is able to be menat the length of the array
+    }
+};
+
+int main() {
+  std::vector<std::vector<int>> testcases = {
+    {1, 1, 2},
+    {0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+  };
+
+  Solution *solution = new Solution();
+
+  for (unsigned int i = 0; i < testcases.size(); i++) {
+    std::cout << solution->removeDuplicates(testcases[i]) << " -> ";
+
+    for (auto j: testcases[i]) {
+      std::cout << j << ' ';
+    }
+
+    std::cout << std::endl;
+  }
+
+  return 0;
+}
+
