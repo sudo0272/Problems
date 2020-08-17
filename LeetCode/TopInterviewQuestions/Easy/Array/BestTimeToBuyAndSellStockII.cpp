dd9131ccce17972de2f1280cad77ae47c8c70067
sudo0272 <sudo180272@protonmail.ch>
2020-08-17 17:03:15 +0900

    Solve problem

--- LeetCode/TopInterviewQuestions/Easy/Array/BestTimeToBuyAndSellStockII.cpp
+++ LeetCode/TopInterviewQuestions/Easy/Array/BestTimeToBuyAndSellStockII.cpp
@@ -3,15 +3,24 @@
 
 class Solution {
   public:
-    int maxProfit(std::vector<int> &prices) {
+    int maxProfit(std::vector<int>& prices) {
+      int profit = 0;
 
+      for (unsigned int i = 1; i < prices.size(); i++) {
+        if (prices[i] > prices[i - 1]) {
+          profit += prices[i] - prices[i - 1];
+        }
+      }
+
+      return profit;
     }
 };
 
 int main() {
   std::vector<int> prices;
   unsigned int pricesSize;
-  unsigned int price;
+  int price;
+  Solution *solution = new Solution();
 
   std::cin >> pricesSize;
 
@@ -21,7 +30,9 @@ int main() {
     prices.push_back(price);
   }
 
-  std::cout << Solution().maxProfit(prices) << std::endl;
+  std::cout << solution->maxProfit(prices) << std::endl;
+
+  delete solution;
 
   return 0;
 }
