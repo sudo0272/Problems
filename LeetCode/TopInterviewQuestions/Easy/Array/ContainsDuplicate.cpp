d857920d44632d08d9ca753a6b3b7986e37f4d1e
sudo0272 <sudo180272@protonmail.ch>
2020-08-19 14:33:11 +0900

    Save nums.size() to numsSize to reduce accessing nums.size()

--- LeetCode/TopInterviewQuestions/Easy/Array/ContainsDuplicate.cpp
+++ LeetCode/TopInterviewQuestions/Easy/Array/ContainsDuplicate.cpp
@@ -4,9 +4,11 @@
 class Solution {
 public:
     bool containsDuplicate(std::vector<int>& nums) {
+      unsigned int numsSize = nums.size();
+
       std::sort(nums.begin(), nums.end());
 
-      for (unsigned int i = 1; i < nums.size(); i++) {
+      for (unsigned int i = 1; i < numsSize; i++) {
         if (nums[i] == nums[i - 1]) {
           return true;
         }
