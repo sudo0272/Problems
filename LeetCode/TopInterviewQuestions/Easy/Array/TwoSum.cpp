0d4393ef5694e505cb68796b785358663fcecd4a
sudo0272 <sudo180272@protonmail.ch>
2020-08-23 01:59:34 +0900

    Solve with using unordered_map

--- LeetCode/TopInterviewQuestions/Easy/Array/TwoSum.cpp
+++ LeetCode/TopInterviewQuestions/Easy/Array/TwoSum.cpp
@@ -1,34 +1,20 @@
 #include <vector>
-#include <algorithm>
+#include <unordered_map>
 
 class Solution {
   public:
     std::vector<int> twoSum(std::vector<int>& nums, int target) {
-      std::vector<int> result;
-      bool isSolutionFound = false;
-      int minNums = *std::min_element(nums.begin(), nums.end());
-      int limit = target - minNums;
-      
-      for (unsigned int i = 0; i < nums.size() && !isSolutionFound; i++) {
-        if (nums[i] > limit) {
-          continue;
-        }
+      std::unordered_map<int, int> indices;
 
-        for (unsigned int j = 0; j < nums.size(); j++) {
-          if (nums[j] > limit || i == j) {
-            continue;
-          }
-
-          if (nums[i] + nums[j] == target) {
-            result.push_back(i);
-            result.push_back(j);
-
-            isSolutionFound = true;
-          }
+      for (int i = 0; i < nums.size(); i++) {
+        if (indices.find(target - nums[i]) != indices.end()) {
+          return {i, indices[target - nums[i]]};
+        } else {
+          indices.insert({nums[i], i});
         }
       }
 
-      return result;
+      return {};
     }
 };
 
