c7c42f5432d549970adc400ca680b1b795543c52
sudo0272 <sudo180272@protonmail.ch>
2020-08-31 18:01:47 +0900

    Free node->next

--- LeetCode/TopInterviewQuestions/Easy/LinkedList/DeleteNodeInALinkedList.c
+++ LeetCode/TopInterviewQuestions/Easy/LinkedList/DeleteNodeInALinkedList.c
@@ -1,11 +1,17 @@
+#include <stdlib.h>
+
 struct ListNode {
   int val;
   struct ListNode *next;
 };
 
 void deleteNode(struct ListNode *node) {
-  node->val = node->next->val;
+  struct ListNode *temp = node->next;
 
-  node->next = node->next->next;
+  node->val = temp->val;
+
+  node->next = temp->next;
+
+  free(temp);
 }
 
